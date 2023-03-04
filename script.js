052cc797d9652758ca6c708e05f54b34a82e7a01
Janindu Nanayakkara <96259895+Janindu-n@users.noreply.github.com>
2023-03-04 17:10:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let insideofCup = document.getElementById("inside")
+/*let insideofCup = document.getElementById("inside")
 insideofCup.addEventListener("click", () => {
     document.getElementById("textbox").innerText="It's the world cup!!!"
-})
+})*/
+let insideofCup = document.getElementById("inside");
+insideofCup.addEventListener("click", () => {
+  let textbox = document.getElementById("textbox");
+  textbox.innerText = "It's the world cup!!!";
+  textbox.style.fontSize = "3em";
+  textbox.style.textAlign = "center";
+});
